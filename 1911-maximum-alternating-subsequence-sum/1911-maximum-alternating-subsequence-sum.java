bb863101e97e89182f034656121f0ed722136294
Amit Verma <152986151+amitvermaio@users.noreply.github.com>
2026-09-03 19:28:07 +0530

    Time: 65 ms (26.88%), Space: 119 MB (54.24%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
@@ -1,27 +1,29 @@
 class Solution {
     long[][] dp;
+    int n;
 
     public long solve(int[] nums, int i, boolean even) {
-        if (i == nums.length) return 0;
-
+        if (i >= n)
+            return 0;
+        
         int flagIdx = even ? 1 : 0;
 
-        if (dp[i][flagIdx] != -1) {
+        if (dp[i][flagIdx] != -1) 
             return dp[i][flagIdx];
-        }
-
+        
         long skip = solve(nums, i+1, even);
-        int val = nums[i];
-        if (!even) val = -val;
-        long take = val + solve(nums, i+1, !even);
+        int num = nums[i];
+        if (!even) num = -num;
+        long take = num + solve(nums, i+1, !even);
 
-        return dp[i][flagIdx] = Math.max(skip, take);
+        return dp[i][flagIdx] = Math.max(take, skip);
     }
 
-    public long maxAlternatingSum(int[] nums) { 
-        dp = new long[nums.length][2];
-        
-        for (int i=0; i<nums.length; i++) {
+    public long maxAlternatingSum(int[] nums) {
+        n = nums.length;
+
+        dp = new long[n+1][2];
+        for (int i=0; i<n+1; i++) {
             dp[i][0] = -1;
             dp[i][1] = -1;
         }
