c757a8b5c853f1d449ac6c6eb3bee35e7f6176c2
Amit Verma <152986151+amitvermaio@users.noreply.github.com>
2026-09-04 12:52:09 +0530

    Time: 403 ms (7.7%), Space: 237.2 MB (14.47%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,17 +1,32 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        List<Integer> tails = new ArrayList<>();
-        for (int num : nums) {
-            int i = Collections.binarySearch(tails, num);
+    int n;
+    int[][] dp;
 
-            if (i < 0) i = -(i+1);
+    private int solve(int[] nums, int i, int prev) {
+        if (i >= n)
+            return 0;
 
-            if (i == tails.size()) {
-                tails.add(num);
-            } else {
-                tails.set(i, num);
-            }
+        if (prev!=-1 && dp[i][prev]!=-1)
+            return dp[i][prev];
+        
+        int skip = solve(nums, i+1, prev);
+        int take = 0;
+        if (prev==-1 || nums[prev]<nums[i]) {
+            take = 1 + solve(nums, i+1, i);
         }
-        return tails.size();
+        if (prev != -1)
+            dp[i][prev] = Math.max(skip, take);
+        return Math.max(skip, take);
+    }
+
+    public int lengthOfLIS(int[] nums) {
+        n = nums.length;
+        dp = new int[n][n];
+
+        for (int i=0; i<n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        return solve(nums, 0, -1);
     }
 }
