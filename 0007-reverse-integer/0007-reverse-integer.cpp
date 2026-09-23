cdafe8ed4712eef67533b7069a6e7fb8ae5827c2
Amit Verma <152986151+amitvermaio@users.noreply.github.com>
2026-09-23 22:27:19 +0530

    Time: 5 ms (19.86%), Space: 8.4 MB (83.11%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -5,7 +5,11 @@ public:
 
         while (n != 0) {
             int dig = n % 10;
-
+            
+            // Niche Wale Step me 10 se multiply kar rhe hain isliye
+            // pehle check kar rhe hain ki agar hamar number currently 
+            // INT_MAX/10 se bada hai to range se bahar chala jayega
+            // to 0 return kar denge and same for INT_MIN/10
             if (revNum > INT_MAX/10 || revNum < INT_MIN/10) {
                 return 0;
             }
